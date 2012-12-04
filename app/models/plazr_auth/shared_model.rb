0634a25246d35f24bf4a51300840ddcdb21a7d31
ampereira <lazeroth89@gmail.com>
2012-12-04 16:26:04 +0000

    Updated database.yml

--- app/models/plazr_auth/shared_model.rb
+++ app/models/plazr_auth/shared_model.rb
@@ -2,10 +2,14 @@ module PlazrAuth
   
   module SharedModel
     extend ActiveSupport::Concern
-
+	
     included do
       class_eval do
         establish_connection "users_#{Rails.env}"
+
+		if Rails.env.production? do
+			config.secondary_database_url = ENV['SECONDARY_DB_URL']
+		end
       end
     end
   end
