728bff9c7b6c9164ab49fc6f5819dd7057b406a7
ampereira <lazeroth89@gmail.com>
2013-01-02 13:47:03 +0000

    shared database config

--- app/models/plazr_auth/shared_model.rb
+++ app/models/plazr_auth/shared_model.rb
@@ -2,11 +2,20 @@ module PlazrAuth
   
   module SharedModel
     extend ActiveSupport::Concern
-	
     included do
-      class_eval do
+	class_eval do
+	  if RAILS.env.production?
+	ActiveRecord::Base.establish_connection( {
+					:adapter => 'mysql2',
+					:host => 'plazrshared.c8eex9xfeq0r.eu-west-1.rds.amazonaws.com',
+					:user => 'plazr',
+					:password => 'plazrplazr',
+					:database => 'plazrshared'
+					})
+	   else
         establish_connection "users_#{Rails.env}"
-      end
+       end
+	  end
     end
   end
 
